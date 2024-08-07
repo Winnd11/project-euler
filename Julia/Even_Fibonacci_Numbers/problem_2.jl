f5c59cab5f64df5af68ebb482b37997cff231101
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-08-07 10:59:04 -0300

    rename folder

--- Julia/Even_Fibonacci_Numbers/problem_2.jl
+++
@@ -1,6 +0,0 @@
-# By considering the terms in the Fibonacci sequence whose values do not exceed four million, find the sum of the even-valued terms.
-# https://projecteuler.net/problem=2
-# ID 2
-
-# 4613732
-
