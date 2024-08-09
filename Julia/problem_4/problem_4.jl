52aade5b46e9598ac829ca18b7487f7207c602d5
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-08-09 17:40:18 -0300

    Add files via upload

---
+++ Julia/problem_4/problem_4.jl
@@ -0,0 +1,23 @@
+# Find the largest palindrome made from the product of two 3-digit numbers.
+# https://projecteuler.net/problem=4
+# ID 4
+
+# 91 X 99 = 9009
+
+# 906609
+
+for i in 90:10000
+    x = string(i)
+    if reverse(x) == x
+        println(i)
+    end
+end
+
+
+# x = 9009
+# for i in 90:100
+#     result = x % i
+#     if result == 0
+#         println(i)
+#     end
+# end
