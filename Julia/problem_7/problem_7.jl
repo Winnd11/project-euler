688629459bf6187a01ba9da10f80de17bb415727
William Rios <133013480+Winnd11@users.noreply.github.com>
2024-10-04 15:29:01 -0300

    Delete .java & .julia

--- Julia/problem_7/problem_7.jl
+++
@@ -1,7 +0,0 @@
-# What is the 10001st prime number?
-# https://projecteuler.net/problem=7
-# ID 7
-
-using Primes
-
-prime(10001)
--- Julia/problem_9/problem_9.jl
+++
@@ -1,5 +0,0 @@
-# There exists exactly one Pythagorean triplet for which a + b + c = 1000 find the product abc.
-# https://projecteuler.net/problem=9
-# ID 9
-
-a = 3^2
